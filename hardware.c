7e2c83e60e07f022d925c7576a420ce932cf9945
Tomas <tomasgonzalez1291@gmail.com>
2026-09-11 13:50:57 -0300

    act

---
+++ hardware.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pwm.h"
+#include "hardware.h"
+#include "tof.h"
+
+//Variable globales
+t_OledParams oled;
+static uint slice;  //del PWM
+static uint channel; //PWM
+
+void init_config(){
+     stdio_init_all();
+
+    // Config OLED
+
+    oled.i2c = i2c1;
+    oled.SDA_PIN = I2C_SDA_PIN;
+    oled.SCL_PIN = I2C_SCL_PIN;
+
+    oled.ctlrType = CTRL_SH1106;
+    oled.i2c_address = OLED_ADDR;
+    oled.height = H_64;
+    oled.width = W_128;
+
+    oledI2cConfig(&oled);
+
+    // Config VL53L0X
+
+    if (!tofInit(1, VL53L0X_ADDR, 0))
+    {
+        printf("Error inicializando VL53L0X\n");
+
+        while (true)
+        {
+            sleep_ms(1000);
+        }
+    }
+
+    printf("VL53L0X inicializado correctamente\n");
+
+    //Config PWM
+    gpio_set_function(SIG_SERVO_PIN, GPIO_FUNC_PWM);
+    slice = pwm_gpio_to_slice_num(SIG_SERVO_PIN);
+    channel = pwm_gpio_to_channel(SIG_SERVO_PIN);
+    pwm_set_clkdiv(slice, SERVO_DIV);
+    pwm_set_wrap(slice, SERVO_TOP);
+    angulo_servo(0);
+    pwm_set_enabled(slice, true);
+}
+
+void angulo_servo(uint angulo)
+{
+    if (angulo > 180) angulo = 180;
+
+    // 1000 us -> 0°
+    // 2000 us -> 180°
+    uint pulse_us = 1000 + (angulo * 1000 / 180);
+
+    // Período = 20000 us
+    uint16_t level = (pulse_us * SERVO_TOP) / 20000;
+
+    pwm_set_chan_level(slice, channel, level);
+}
+
---
+++ hardware.h
@@ -0,0 +1,25 @@
+#ifndef HARDWARE_H
+#define HARDWARE_H
+
+#include "picoOled.h"
+
+//Variables
+extern t_OledParams oled;
+
+//I2C
+#define I2C_SDA_PIN         14
+#define I2C_SCL_PIN         15
+#define OLED_ADDR           0x3C
+#define VL53L0X_ADDR        0x29
+
+//PWM
+#define SIG_SERVO_PIN       16
+#define SERVO_FREQ          50
+#define SERVO_TOP           59999
+#define SERVO_DIV           50.0f
+
+//Funciones
+void init_config();
+void angulo_servo(uint angulo);
+
+#endif
